1ef7b897a601deaf18a9900cf4b991122e711fd4
Aidan Fitzgerald <duckjr159@gmail.com>
2016-04-17 09:25:51 -0400

    Integer division is faster; avoid floating points

--- core.py
+++ core.py
@@ -11,6 +11,5 @@ def avg_color(blocks): #it averages every color in each block structure the same
 def avg_helper(block):
     h, w, d = block.shape #sets a variable for the 3 dimensional array
     S = np.sum(block, axis=(0, 1), dtype = np.dtype(np.uint16)) #add all the elements in each row of the array or each column
-    mean = S / (h*w) # returns an array of 3 numbers, avg red, g, and blue
-    # Round, convert back to uint8, and tile to shape of original block
-    return pixel_to_block(np.rint(mean).astype(np.dtype(np.uint8)), w, h)
+    # Integer-divide, convert back to uint8, and tile to shape of original block
+    return pixel_to_block((S // (h*w)).astype(np.dtype(np.uint8)), w, h)
