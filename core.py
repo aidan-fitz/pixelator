662d46613d91fac780063405573ee4b55eab2640
huangje1 <jennyhuang@BridgeUp.local>
2016-04-17 07:40:20 -0400

    core edit

--- core.py
+++ core.py
@@ -1,9 +1,11 @@
 import numpy as np
 from utils import *
 
-def avg_color(blocks):
-    temp = upgrade(blocks[0][0])
-    # complete this function
+def avg_color(blocks): #it averages every color in each block structure the same
+	# temp = upgrade(blocks[0][0])
+    for row in range(len(blocks)): 
+     	for column in range(len(blocks[row])):
+    		blocks[row][column] = avg_helper(blocks[row][column])
 #helper function that averages a single block
 def avg_helper(block):
 	h, w, d = block.shape #sets a variable for the 3 dimensional array
