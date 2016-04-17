bbd3fbe6a92585286760e1278d684019db389044
Aidan Fitzgerald <duckjr159@gmail.com>
2016-04-16 23:54:51 -0400

    Moved command line parser to cmd.py module

--- pixelate.py
+++ pixelate.py
@@ -1,27 +1,8 @@
 import numpy as np
 from scipy.misc import imread, imsave
-from sys import argv
-from error import FileNotGivenError
+import cmd
 
-fname_in = None
-fname_out = None
+fin, fout = cmd.files()
 
-try:
-    args = iter(argv[1:])
-    while True:
-        nxt = next(args)
-        if nxt == '-i':
-            fname_in = next(args)
-        elif nxt == '-o':
-            fname_out = next(args)
-        else:
-            fname_in = nxt
-except StopIteration:
-    if fname_in is None:
-        raise FileNotGivenError('I need a file name')
-    elif fname_out is None:
-        index = fname_in.rfind('.')
-        fname_out = fname_in[:index] + '-pxl8d' + fname_in[index:]
-
-print(fname_in)
-print(fname_out)
+print(fin)
+print(fout)
