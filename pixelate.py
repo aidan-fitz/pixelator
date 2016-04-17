8349baf812d42f697b28f864a67a7efdfd982c31
Aidan Fitzgerald <duckjr159@gmail.com>
2016-04-17 05:00:29 -0400

    Finished chunk()

--- pixelate.py
+++ pixelate.py
@@ -16,20 +16,7 @@ image = imread(fin)
 print(image.dtype)
 print(image.shape)
 
-maxrow = lambda row: min(row + 8, image.shape[0])
-maxcol = lambda col: min(col + 8, image.shape[1])
+blocks = chunk(image)
 
-blocks = [ \
-            [[row, col, maxrow(row), maxcol(col)] \
-            for row in range(0, image.shape[0], 8)] \
-        for col in range(0, image.shape[1], 8)]
-
-print(blocks[0][1])
-print(blocks[1][1])
-print(blocks[-1][-1])
-
-'''
-block = image[:8, :8]
-
-print(block.shape)
-'''
+print(blocks[0][0].shape)
+print(blocks[-1][-1].shape)
--- utils.py
+++ utils.py
@@ -8,10 +8,10 @@ def downgrade(array):
 
 def chunk(array, dx = 16, dy = 16):
     if dx * dy >= 257:
-        raise ValueError("Intermediate sums need to fit inside uint16!  A maximum of 257 uint8's can be added together.")
+        raise ValueError("Intermediate sums need to fit inside uint16!  A maximum of 257 uint8's can be added together, and %d * %d == %d won't fit." % (dx, dy, dx * dy))
 
-    maxrow = lambda row: min(row + dy, image.shape[0])
-    maxcol = lambda col: min(col + dx, image.shape[1])
+    maxrow = lambda row: min(row + dy, array.shape[0])
+    maxcol = lambda col: min(col + dx, array.shape[1])
 
-    blocks = [ [ array[row : maxrow(row), col : maxcol(col)] for col in range(0, image.shape[1], dx) ] for row in range(0, image.shape[0], dy) ]
+    blocks = [ [ array[row : maxrow(row), col : maxcol(col)] for col in range(0, array.shape[1], dx) ] for row in range(0, array.shape[0], dy) ]
     return blocks
